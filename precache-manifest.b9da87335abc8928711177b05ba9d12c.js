b7b2bf53d5a44474f8ddfcb72360fd53a41fb674
taras <stvstudio.com.ua@gmail.com>
2019-10-09 17:13:17 +0300

    Updates

---
+++ precache-manifest.b9da87335abc8928711177b05ba9d12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c6e36ca000c5089bc73edad5db380b",
+    "url": "/goit-react-hw-03-bank-account/./index.html"
+  },
+  {
+    "revision": "e9eae0385a54060811b9",
+    "url": "/goit-react-hw-03-bank-account/./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "7824175f73b08d850334",
+    "url": "/goit-react-hw-03-bank-account/./static/css/main.46b89725.chunk.css"
+  },
+  {
+    "revision": "e9eae0385a54060811b9",
+    "url": "/goit-react-hw-03-bank-account/./static/js/2.e14d96f7.chunk.js"
+  },
+  {
+    "revision": "7824175f73b08d850334",
+    "url": "/goit-react-hw-03-bank-account/./static/js/main.25169271.chunk.js"
+  },
+  {
+    "revision": "ab1da02108790b4e3474",
+    "url": "/goit-react-hw-03-bank-account/./static/js/runtime-main.651549df.js"
+  }
+]);
